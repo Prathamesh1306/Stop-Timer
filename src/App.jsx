048e35337f4cc06d230f1434c63fb76b5d04c5a5
Prathamesh1306 <arceusgaming13@gmail.com>
2025-06-02 22:16:37 +0530

    Added pause and reset button implement the stopwatch by javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,77 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [time, Time] = useState(0);
+  const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
-  const [isReset, setIsReset] = useState(false);
-  const [isStarted, setIsStarted] = useState(false);
-  const [isStopped, setIsStopped] = useState(false);
+  const [isPaused, setIsPaused] = useState(false); // It is for pause as well as for the resume button
+
+
+  // the useffect is run when the any changes occurs in dependacies(isrunning and ispaused)
+  useEffect(()=>{
+    let timer;
+    if(isRunning && !isPaused){
+    timer=setInterval(()=> setTime(t=>t+1),1000);
+    }
+    return () => clearInterval(timer);
+  },[isRunning,isPaused]);
 
   
+  const format = (time) => `${String(Math.floor(time / 60))}:${String(time % 60)}`;
+
+
+  const start = () =>{
+    setIsRunning(true);
+    setIsPaused(false);
+  }
+  
+  const stop = () =>{
+    setIsRunning(false);
+  }
+  
+  
+  const reset = () =>{
+    setIsRunning(false);
+    setTime(0);
+    setIsPaused(false);
+  }
+
+  const paused = () =>{
+     setIsPaused(true);
+  }
+ 
+  const resume = () =>{
+    if(isPaused && isRunning){
+      setIsPaused(false);
+    }
+  }
+
   return (
     <div className="App">
 
       <div className='Timer'>      
-        <h1>da</h1>
+        <h1>{format(time)}</h1>
       </div>
+
       <div className="buttons">
-        <button onClick={'myvar=set'}>
+        
+        <button onClick={start}>
           Start 
         </button>
         
-        <button>
+        <button onClick={stop}> 
          Stop
         </button>
 
-        <button>
+        <button onClick={paused} disabled={!isRunning||isPaused}> 
+         Pause
+        </button>
+
+        <button onClick={resume} disabled={!isRunning || !isPaused}> 
+         resume
+        </button>
+
+        <button onClick={reset}>
           Reset
         </button>
 
